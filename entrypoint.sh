46f4bab3538ad0b28c807fce71c95f12095b4ec4
Roger Steve Ruiz <hi@rog.gr>
2022-12-11 01:37:27 -0500

    Let's be safe about ownership

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+git config --global --add safe.directory /github/workspace
 git config --global commit.gpgsign true
 git config --global tag.gpgsign true
 git config --global gpg.format ssh
